4476ff69799b877fc6b2b581547e3b7f7f9b0bc4
Maria Eduarda <136341978+marianogueirx@users.noreply.github.com>
2025-05-02 11:37:40 -0400

    Add files via upload

---
+++ src/README.md
@@ -0,0 +1,5 @@
+# Projeto Star Wars
+
+Desenvolvido por: Maria Eduarda Nogueira e Isabelly Cristina
+
+Servidor com Node.js e Express.
---
+++ src/server.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'pages/index.html')));
+app.get('/sobre', (req, res) => res.sendFile(path.join(__dirname, 'pages/sobre.html')));
+app.get('/comprar', (req, res) => res.sendFile(path.join(__dirname, 'pages/comprar.html')));
+app.get('/sinopse', (req, res) => res.sendFile(path.join(__dirname, 'pages/sinopse.html')));
+app.get('/atores', (req, res) => res.sendFile(path.join(__dirname, 'pages/atores.html')));
+app.get('/opiniao', (req, res) => res.sendFile(path.join(__dirname, 'pages/opiniao.html')));
+
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
